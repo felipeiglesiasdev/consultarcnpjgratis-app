6f5f3028c4c0c9084e69263bbf95fe7cf36ebbfe
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-10-20 10:19:04 -0300

    tag google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP412M62C7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-KP412M62C7');
+    </script>
 </head>
 <body class="font-sans antialiased">
     <div class="flex flex-col min-h-screen">
